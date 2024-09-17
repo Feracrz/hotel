c3a4c2292696dee0798dab3ad5b3aa30287c86b3
FeraCrz <ferota1234@gmail.com>
2024-09-17 15:01:58 -0600

    subiendo

--- app.js
+++ app.js
@@ -1,51 +1,68 @@
 let imagenes = [{
-        "url": "img/hero.jpg",
+        "url": "img/img1.webp",
         "nombre": "Proyecto 01",
-        "descripcion": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever"
+        "descripcion": "Este es el proyecto 01 fue desarrollado para un video de youtube. Si te gusta el contenido dale like y suscribete"
+
     },
     {
-        "url": "img/hero.jpg",
-        "nombre": "Proyecto 01",
-        "descripcion": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever"
+        "url": "img/img2.webp",
+        "nombre": "Proyecto 02",
+        "descripcion": "Hola a todos este es el proyecto02 y fue desarrollado para un video de youtube. Si te gusta el contenido dale like y suscribete"
+
     },
     {
-        "url": "img/hero.jpg",
-        "nombre": "Proyecto 01",
-        "descripcion": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever"
+        "url": "img/img3.webp",
+        "nombre": "Proyecto 03",
+        "descripcion": "Este proyecto, es el 03 y fue desarrollado para un video de youtube. Si te gusta el contenido dale like y suscribete"
+
     },
 ]
+
 
 let atras = document.getElementById('atras');
 let adelante = document.getElementById('adelante');
 let imagen = document.getElementById('img');
 let puntos = document.getElementById('puntos');
-let texto = document.getElementById('texto-c');
-let actual = 0;
+let texto = document.getElementById('texto')
+let actual = 0
+posicionCarrusel()
 
 atras.addEventListener('click', function() {
     actual -= 1
+
     if (actual == -1) {
         actual = imagenes.length - 1
     }
-    imagen.innerHTML = `<img src="img/hero.jpg" alt="imagen1" class="img" loading="lazy">`
+
+    imagen.innerHTML = ` <img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`
     texto.innerHTML = `
     <h3>${imagenes[actual].nombre}</h3>
-    <p>${imagenes[actual].nombre}</p>
+    <p>${imagenes[actual].descripcion}</p>
     `
+    posicionCarrusel()
 })
 adelante.addEventListener('click', function() {
     actual += 1
+
     if (actual == imagenes.length) {
         actual = 0
     }
-    imagen.innerHTML = `<img src="img/hero.jpg" alt="imagen1" class="img" loading="lazy">`
+
+    imagen.innerHTML = ` <img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`
     texto.innerHTML = `
     <h3>${imagenes[actual].nombre}</h3>
-    <p>${imagenes[actual].nombre}</p>
+    <p>${imagenes[actual].descripcion}</p>
     `
+    posicionCarrusel()
 })
-fuction posicionCarrusel() {
-    for (var i = 0; i < imagenes.length; i++)
-        if >
 
+function posicionCarrusel() {
+    puntos.innerHTML = ""
+    for (var i = 0; i < imagenes.length; i++) {
+        if (i == actual) {
+            puntos.innerHTML += '<p class="bold">.<p>'
+        } else {
+            puntos.innerHTML += '<p>.<p>'
+        }
+    }
 }
